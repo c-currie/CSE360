72682dd956b41e3249f6cf5bb31a1daf4fa9902e
codycurrie <codycurrie@codys-mbp.lan>
2020-10-07 21:20:44 -0700

    Initial commit

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,66 @@
+package cse360assignment02;
+
+/**
+ * The AddingMachine program implements an application that simply adds and
+ * subtracts an int value, the program then prints out all of the arithmetic
+ * history.
+ *
+ * @author Cody Currie
+ * @version 1.0
+ * @since 2020-10-7
+ */
+
+public class AddingMachine {
+	/**
+	 * Total value at any given time.
+	 */
+	private int total;
+
+	/**
+	 * This class creates an AddingMachine object that can add and subtract an int
+	 * value.
+	 */
+	public AddingMachine() {
+		total = 0; // not needed - included for clarity
+	}
+
+	/**
+	 * TODO Returns the current total.
+	 */
+	public int getTotal() {
+		return 0;
+	}
+
+	/**
+	 * TODO Adds an int value to the current total.
+	 * 
+	 * @param value
+	 */
+	public void add(int value) {
+
+	}
+
+	/**
+	 * TODO Subtracts a value from the current total.
+	 * 
+	 * @param value
+	 */
+	public void subtract(int value) {
+
+	}
+
+	/**
+	 * TODO Prints out a formatted String representation of the operations and
+	 * values.
+	 */
+	public String toString() {
+		return "";
+	}
+
+	/**
+	 * TODO Clears all of the current operations from the String
+	 */
+	public void clear() {
+
+	}
+}
